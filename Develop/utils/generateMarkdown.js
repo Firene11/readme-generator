e7c540b136909ed803b183ed68d83bbda96516dc
Fih <124883284+Firene11@users.noreply.github.com>
2023-07-02 18:51:16 -0300

    cleaned up comments and code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,5 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// A function that returns a license badge based on which license is passed in. 
+
 function renderLicenseBadge(license) {
   if (license === 'Apache 2.0') {
     return 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
@@ -10,8 +10,8 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// A function that returns the license link
+
 function renderLicenseLink(license) {
   if (license === 'Apache 2.0') {
     return 'https://opensource.org/licenses/Apache-2.0'
@@ -22,15 +22,16 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// A function that returns the license section of README
+
 function renderLicenseSection(license) {
   return license === 'None'
   ? ''
   : `[![License: ${license}](${renderLicenseBadge(license)})](${renderLicenseLink(license)})`
 }
 
-// TODO: Create a function to generate markdown for README
+// A function to generate markdown for README
+
 function generateMarkdown(data) {
   return `
   # ${data.title}
